3a71dc9e34506d4221ab50026c5ff5a7f605fb3a
wayne <pringlewood@gmail.com>
2024-08-29 06:53:40 +0200

    fixes

--- src/eradius_log.erl
+++ src/eradius_log.erl
@@ -265,26 +265,6 @@ escape_char($") -> <<"\\\"">>;
 escape_char(C) when C >= 32, C < 127 -> <<C>>;
 escape_char(C) -> <<"\\", (i2b(C))/binary>>.
 
-day(1) -> "Mon";
-day(2) -> "Tue";
-day(3) -> "Wed";
-day(4) -> "Thu";
-day(5) -> "Fri";
-day(6) -> "Sat";
-day(7) -> "Sun".
-
-month(1) -> "Jan";
-month(2) -> "Feb";
-month(3) -> "Mar";
-month(4) -> "Apr";
-month(5) -> "May";
-month(6) -> "Jun";
-month(7) -> "Jul";
-month(8) -> "Aug";
-month(9) -> "Sep";
-month(10) -> "Oct";
-month(11) -> "Nov";
-month(12) -> "Dec".
 
 -compile({inline, i2b/1}).
 i2b(I) -> list_to_binary(integer_to_list(I)).
